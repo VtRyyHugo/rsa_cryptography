1aa8dcb9dc5553fc60185c2c775b9430f0a2d463
Vitor Hugo <vitorhugo929@gmail.com>
2019-04-16 12:00:52 -0300

    Criando projeto de Criptografia RSA

---
+++ RsaCryptography/Crypyography.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RsaCryptography
+{
+    class Crypyography
+    {
+        public void generateBytes() {
+            string str = Console.ReadLine();
+
+            byte[] codes = Encoding.ASCII.GetBytes(str);
+        }
+    }
+}
---
+++ RsaCryptography/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RsaCryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
